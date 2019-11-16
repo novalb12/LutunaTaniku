e8eeb609271bccf3f86144a11604fa3b7cc767bc
novalb12 <novalb65@gmail.com>
2019-11-16 21:35:57 +0700

    bom

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+SELAMAT MASUK {{Auth::user()->name ?? ''}}
+
+<br>
+<a href="{{route('logout')}}">Log Out</a>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/admin','AdminController@index');
+Route::get('/register','AuthController@getRegister')->middleware('guest')->name('register');
+Route::post('/register','AuthController@postRegister')->middleware('guest');
+Route::get('/login','AuthController@getLogin')->middleware('guest')->name('login');
+Route::post('/login','AuthController@postLogin')->middleware('guest');
+Route::get('/home',function(){
+    return view('home');
+})->middleware('auth')->name('home');
+Route::get('/logout','AuthController@logout')->middleware('auth')->name('logout');
